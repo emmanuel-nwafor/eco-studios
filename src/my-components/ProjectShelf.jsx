846b9574cfac37b09ce70ad674f1e16a3f0de41a
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-04-07 01:57:02 -0700

    all commit

--- src/my-components/ProjectShelf.jsx
+++ src/my-components/ProjectShelf.jsx
@@ -1,110 +1,110 @@
 import React from 'react';
 
 const items = [
-    {
-      id: 1,
-      title: 'Attendance App',
-      link: 'https://attendance-system-sable.vercel.app/',
-      image: '/src/assets/attendance.png',
-      description:
-        'A facial recognition system for marking student attendance with Firebase support.',
-      techStack: [
-        { icon: 'bxl-react', color: '#61DBFB' },
-        { icon: 'bxl-firebase', color: '#FFA611' },
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-    {
-      id: 2,
-      title: 'Banking Finance App',
-      link: 'https://banking-app-tawny-pi.vercel.app/',
-      image: '/src/assets/banking.png',
-      description:
-        'A modern banking interface for handling transactions, savings, and user analytics.',
-      techStack: [
-        { icon: 'bxl-react', color: '#61DBFB' },
-        { icon: 'bxl-firebase', color: '#FFA611' },
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-    {
-      id: 3,
-      title: 'Jobbie',
-      link: 'https://jobbie-six.vercel.app/',
-      image: '/src/assets/jobbie.png',
-      description:
-        'A job portal where recruiters post jobs and seekers apply with a simple interface.',
-      techStack: [
-        { icon: 'bxl-react', color: '#61DBFB' },
-        { icon: 'bxl-firebase', color: '#FFA611' },
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-    {
-      id: 4,
-      title: 'Otto-Sons',
-      link: 'https://otto-sons-web-app.vercel.app/',
-      image: '/src/assets/otto-sons.png',
-      description:
-        'A car rental, repair, and selling web app using local storage for data persistence.',
-      techStack: [
-        { icon: 'bxl-react', color: '#61DBFB' },
-        { icon: 'bxl-firebase', color: '#FFA611' },
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-    {
-      id: 5,
-      title: 'TV Hub',
-      link: 'https://tvh-woad.vercel.app/',
-      image: '/src/assets/tvh.png',
-      description:
-        'A simple but yet high performance website, where users can search for movies, view the casts and watch trailers of the movie searched.',
-      techStack: [
-        { icon: 'bxl-html5', color: '#E44D26' },
-        { icon: 'bxl-css3', color: '#1572B6' }, 
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-    {
-      id: 6,
-      title: 'La Madison Agency',
-      link: 'https://emmanuel-nwafor.github.io/La-Madison-Agency/',
-      image: '/src/assets/la-madison.png',
-      description:
-        'A simple but yet high performance website, where users can search for movies, view the casts and watch trailers of the movie searched.',
-      techStack: [
-        { icon: 'bxl-html5', color: '#E44D26' },
-        { icon: 'bxl-css3', color: '#1572B6' }, 
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-    {
-      id: 7,
-      title: 'Jeju Store',
-      link: 'https://emmanuel-nwafor.github.io/La-Madison-Agency/',
-      image: '/src/assets/jeju.png',
-      description:
-        'An e-commerce website for a store selling products, with a focus on user experience, perfomance and responsiveness.',
-      techStack: [
-        { icon: 'bxl-react', color: '#61DBFB' },
-        { icon: 'bxl-css3', color: '#1572B6' }, 
-        { icon: 'bxl-tailwind-css', color: '#38BDF8' },
-        { icon: 'bxl-javascript', color: '#F0DB4F' },
-      ],
-    },
-  ];
+  {
+    id: 1,
+    title: 'Attendance App',
+    link: 'https://attendance-system-sable.vercel.app/',
+    image: '/src/assets/attendance.jpg',
+    description:
+      'A facial recognition system for marking student attendance with Firebase support.',
+    techStack: [
+      { icon: 'bxl-react', color: '#61DBFB' },
+      { icon: 'bxl-firebase', color: '#FFA611' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Banking Finance App',
+    link: 'https://banking-app-tawny-pi.vercel.app/',
+    image: '/src/assets/banking.jpg',
+    description:
+      'A modern banking interface for handling transactions, savings, and user analytics.',
+    techStack: [
+      { icon: 'bxl-react', color: '#61DBFB' },
+      { icon: 'bxl-firebase', color: '#FFA611' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Jobbie',
+    link: 'https://jobbie-six.vercel.app/',
+    image: '/src/assets/jobbie.jpg',
+    description:
+      'A job portal where recruiters post jobs and seekers apply with a simple interface.',
+    techStack: [
+      { icon: 'bxl-react', color: '#61DBFB' },
+      { icon: 'bxl-firebase', color: '#FFA611' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Otto-Sons',
+    link: 'https://otto-sons-web-app.vercel.app/',
+    image: '/src/assets/otto-sons.jpg',
+    description:
+      'A car rental, repair, and selling web app using local storage for data persistence.',
+    techStack: [
+      { icon: 'bxl-react', color: '#61DBFB' },
+      { icon: 'bxl-firebase', color: '#FFA611' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+  {
+    id: 5,
+    title: 'TV Hub',
+    link: 'https://tvh-woad.vercel.app/',
+    image: '/src/assets/tvh.jpg',
+    description:
+      'A simple but yet high performance website, where users can search for movies, view the casts and watch trailers of the movie searched.',
+    techStack: [
+      { icon: 'bxl-html5', color: '#E44D26' },
+      { icon: 'bxl-css3', color: '#1572B6' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+  {
+    id: 6,
+    title: 'La Madison Agency',
+    link: 'https://emmanuel-nwafor.github.io/La-Madison-Agency/',
+    image: '/src/assets/la-madison.jpg',
+    description:
+      'A simple but yet high performance website, where users can search for movies, view the casts and watch trailers of the movie searched.',
+    techStack: [
+      { icon: 'bxl-html5', color: '#E44D26' },
+      { icon: 'bxl-css3', color: '#1572B6' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+  {
+    id: 7,
+    title: 'Jeju Store',
+    link: 'https://emmanuel-nwafor.github.io/La-Madison-Agency/',
+    image: '/src/assets/jeju.jpg',
+    description:
+      'An e-commerce website for a store selling products, with a focus on user experience, perfomance and responsiveness.',
+    techStack: [
+      { icon: 'bxl-react', color: '#61DBFB' },
+      { icon: 'bxl-css3', color: '#1572B6' },
+      { icon: 'bxl-tailwind-css', color: '#38BDF8' },
+      { icon: 'bxl-javascript', color: '#F0DB4F' },
+    ],
+  },
+];
 
 const ProjectShelf = () => {
   return (
     <div className="bg-black text-white p-9">
-      <h2 className="text-3xl font-bold mb-8 text-center">{"<>"}Project Shelf{"</>"}</h2>
+      <h2 className="text-3xl font-bold mb-8 text-center">{'<>'}Project Shelf{'</>'}</h2>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 max-w-6xl mx-auto mt-8">
         {items.map((item) => (
           <a
@@ -141,5 +141,4 @@ const ProjectShelf = () => {
   );
 };
 
-// Make sure to export the component as default
 export default ProjectShelf;
